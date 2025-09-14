d12d7bd7df2d416bba0d8299cb65b0156e6ec40e
Ansh <141398134+akarvir@users.noreply.github.com>
2025-09-14 08:32:27 -0400

    set correct env variables

--- app/main.py
+++ app/main.py
@@ -14,7 +14,6 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://startstudying.netlify.app"
-    # add deployed frontend URL later, e.g. "https://studyhub.netlify.app"
 ]
 
 app.add_middleware(
--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 
-const SUPABASE_URL = "https://jnkqrosvfjsrvtkumazf.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impua3Fyb3N2ZmpzcnZ0a3VtYXpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1NTAzNjMsImV4cCI6MjA3MzEyNjM2M30.613B5-FlYGl0lUCj_eHNyix9U13Jx9AkWSfkaW_UPds"
-console.log("Supabase URL:", SUPABASE_URL)
-console.log("Supabase Key defined:", !!SUPABASE_ANON_KEY)
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
